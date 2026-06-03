3a32f9b3888c7d842b8a37c6187960e392f63e7f
plasmabucket <286383116+plasmabucket@users.noreply.github.com>
2026-06-04 00:16:57 +0400

    Left sorted() function in use only for random test

--- solutions/task-12/sort_tests.py
+++ solutions/task-12/sort_tests.py
@@ -9,11 +9,17 @@ class SortTests(unittest.TestCase):
     def test_regression(self) -> None:
         # Проверка, что лист сортируется
         unsorted_list: List = [2, 8, 9, 7, 10, 6, 1, 4, 5, 3]
-        sorted_list: List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        self.assertEqual(list_sort(unsorted_list), sorted_list,
+        self.assertEqual(list_sort(unsorted_list),
+                         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                          "list didn't get sorted")
-        # Проверка, что отсортированный лист не изменяется
-        self.assertEqual(list_sort(sorted_list), sorted_list,
+        # Проверка, что переданный лист не был изменён
+        self.assertEqual(unsorted_list,
+                         [2, 8, 9, 7, 10, 6, 1, 4, 5, 3],
+                         "parameter got modified")
+        # Проверка, что отсортированный лист остаётся отсортированным
+        sorted_list: List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.assertEqual(list_sort(sorted_list),
+                         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                          "sorted list got unsorted")
 
     def test_random(self) -> None:
@@ -30,13 +36,14 @@ class SortTests(unittest.TestCase):
     def test_empty(self) -> None:
         # Проверяем сортировку пустого списка
         unsorted_list: List = []
-        self.assertEqual(list_sort(unsorted_list), sorted(unsorted_list),
+        self.assertEqual(list_sort(unsorted_list), [],
                          "empty list is unhandled")
 
     def test_max(self) -> None:
         # Проверяем сортировку больших значений
         unsorted_list: List = [9223372036854775808, -9223372036854775808, 0]
-        self.assertEqual(list_sort(unsorted_list), sorted(unsorted_list),
+        self.assertEqual(list_sort(unsorted_list),
+                         [-9223372036854775808, 0, 9223372036854775808],
                          "max values aren't sorted")
 
 
